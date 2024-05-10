d9b2ff7fff59e7cac9c929c8d5737a4559d46169
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-05-10 14:45:24 +0900

    feat: Add user insertion functionality to RecordPage

---
+++ app/api/record/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: Request, res: NextResponse) => {
+  const { name, email } = await req.json();
+
+  const new_user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  return NextResponse.json(new_user);
+};
--- app/record/page.tsx
+++ app/record/page.tsx
@@ -1,11 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
-const RecordPage = () => {
+const RecordPage = async () => {
+  const handleClick = async () => {
+    const response = await fetch("/api/record", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "John Doe",
+        email: "hoge",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="conatiner m-auto">
       <div className="flex flex-col h-screen items-center justify-center">
         <p>RecordPage</p>
+        <button onClick={handleClick} className="my-10 p-2 bg-black text-white">
+          INSERT user
+        </button>
         <Link href="/">Go back</Link>
       </div>
     </div>
